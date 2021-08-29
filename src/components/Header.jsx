0fc44e6503d02371b5c21eba98563c1892bae6be
Matt Kaye <mkaye@durst.org>
2021-08-28 20:09:17 -0400

    Mobile navigation work.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,20 @@ import '../scss/components/navigation.scss';
 export const Header = () => {
   const [navIsActive, setNavActiveState] = useState(false);
 
-  function toggleNavState() {
-    setNavActiveState(!navIsActive ? true : false);
+  function toggleNavState(e) {
+    if (e.target.dataset.logo) {
+      setNavActiveState(false);
+    } else {
+      setNavActiveState(!navIsActive ? true : false);
+    }
   }
 
   return (
     <header>
-      <p>{}</p>
       <nav>
         <button
           className={
-            'hamburger hamburger--spring' + (navIsActive ? ' is-active' : '')
+            'hamburger hamburger--boring' + (navIsActive ? ' is-active' : '')
           }
           type="button"
           aria-label="Menu"
@@ -27,74 +30,82 @@ export const Header = () => {
             <span className="hamburger-inner"></span>
           </span>
         </button>
-        <div className="w-full">
-          <ul className="xl:gap-32 md:gap-10">
+        <div className={'nav-wrapper ' + (navIsActive ? ' is-active' : '')}>
+          <div className="w-full nav-section">
+            <ul className="lg:gap-16 md:gap-0" onClick={toggleNavState}>
+              <li>
+                <NavLink activeClassName="selected" to="/news">
+                  News
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="selected" to="/discography">
+                  Discography
+                </NavLink>
+              </li>
+
+              <li className="item-has-children">
+                <NavLink activeClassName="selected" to="/band">
+                  Band
+                </NavLink>
+                <ul>
+                  <li>
+                    <NavLink activeClassName="selected" to="/band/geddy-lee">
+                      Geddy
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink activeClassName="selected" to="/band/alex-lifeson">
+                      Alex
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink activeClassName="selected" to="/band/neil-peart">
+                      Neil
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <ul className="rash-logo">
             <li>
-              <NavLink activeClassName="selected" to="/news">
-                News
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="selected" to="/tour">
-                Tour
-              </NavLink>
-            </li>
-            <li className="item-has-children">
-              <NavLink activeClassName="selected" to="/band">
-                Band
-              </NavLink>
-              <ul>
-                <li>
-                  <NavLink activeClassName="selected" to="/band/geddy-lee">
-                    Geddy
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink activeClassName="selected" to="/band/alex-lifeson">
-                    Alex
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink activeClassName="selected" to="/band/neil-peart">
-                    Neil
-                  </NavLink>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-        <ul className="rash-logo">
-          <li>
-            <NavLink to="/">
-              <img src={logo} alt="" />
-            </NavLink>
-          </li>
-        </ul>
-        <div className="w-full">
-          <ul className="xl:gap-16 md:gap-6">
-            <li>
-              <NavLink activeClassName="selected" to="/discography">
-                Discography
-              </NavLink>
-            </li>
-            <li>
-              <a
-                href="http://www.rushbackstage.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Shop
-              </a>
-            </li>
-            <li>
-              <NavLink
-                activeClassName="selected"
-                to="/professors-word-scramble"
-              >
-                Lyric Scramble
+              <NavLink to="/">
+                <img
+                  src={logo}
+                  alt="Rash Logo"
+                  data-logo
+                  onClick={toggleNavState}
+                />
               </NavLink>
             </li>
           </ul>
+          <div className="w-full nav-section">
+            <ul className="lg:gap-16 md:gap-0" onClick={toggleNavState}>
+              <li>
+                <NavLink activeClassName="selected" to="/tour">
+                  Tour
+                </NavLink>
+              </li>
+              <li>
+                <a
+                  href="http://www.rushbackstage.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Shop
+                </a>
+              </li>
+              <li>
+                <NavLink
+                  activeClassName="selected"
+                  to="/professors-word-scramble"
+                >
+                  Lyric Scramble
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </header>
